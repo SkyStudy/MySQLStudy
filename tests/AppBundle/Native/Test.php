356bc7a19760f86c00d1f2159aab7038ca5b8a10
SkyInterBy <soJIo1010@mail.ru>
2016-02-27 21:29:59 +0200

    test join

--- tests/AppBundle/Native/Test.php
+++ tests/AppBundle/Native/Test.php
@@ -603,6 +603,76 @@ class Test extends AbstractTest
         $this->clear(['s_user']);
     }
 
+    public function testJoin()
+    {
+        $connection = $this->getConnection();
+
+        try {
+            $connection->exec('
+                CREATE TABLE `s_product`(
+                  `code` INT,
+                  `name` VARCHAR(255)
+                );
+            ');
+
+            $connection->exec('
+                CREATE TABLE `s_warehouse`(
+                  `product_code` INT,
+                  `price` INT,
+                  `quantity` INT
+                );
+            ');
+
+            $connection->exec("
+                INSERT INTO `s_product`(`code`, `name`)
+                VALUES
+                  (1, 'Green Shirt'),
+                  (2, 'White Shirt'),
+                  (3, 'Red Shirt');
+            ");
+
+            $connection->exec("
+                INSERT INTO `s_warehouse`(`product_code`, `price`, `quantity`)
+                VALUES
+                  (1, 120, 8),
+                  (2, 125, 5),
+                  (3, 200, 0);
+            ");
+
+            $result = $connection->fetchAll('
+                SELECT * FROM `s_product`
+                JOIN `s_warehouse` ON (`product_code` = `code`)
+            ');
+
+            $expect = [
+                [
+                    'code' => '1',
+                    'name' => 'Green Shirt',
+                    'product_code' => '1',
+                    'price' => '120',
+                    'quantity' => '8',
+                ],
+                [
+                    'code' => '2',
+                    'name' => 'White Shirt',
+                    'product_code' => '2',
+                    'price' => '125',
+                    'quantity' => '5',
+                ],
+                [
+                    'code' => '3',
+                    'name' => 'Red Shirt',
+                    'product_code' => '3',
+                    'price' => '200',
+                    'quantity' => '0',
+                ]
+            ];
+
+            $this->assertSame($result, $expect);
+        } finally {
+            $this->clear(['s_product', 's_warehouse']);
+        }
+    }
 
     public function testStudents()
     {

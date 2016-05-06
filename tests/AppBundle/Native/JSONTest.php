569584b473a8203eca1efdb4f2f08bc4a615305a
ReenExe <ipourri@gmail.com>
2016-05-06 19:19:13 +0300

    try build

---
+++ tests/AppBundle/Native/JSONTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\AppBundle\Native;
+
+class JSONTest extends ConnectionTestCase
+{
+    public function testObject()
+    {
+        $connection = $this->getConnection();
+
+        $result = $connection->fetchColumn(<<<'SQL'
+          SELECT JSON_OBJECT('id', 1);
+SQL
+        );
+
+        $this->assertSame($result, '{"id":1}');
+    }
+}
